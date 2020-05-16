32f86499b00787fc7dbb55b27c43b81c0487d90c
BrennenMcGill <31702142+BrennenMcGill@users.noreply.github.com>
2020-05-15 22:43:49 -0600

    initail prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,41 @@
 // Assignment code here
 
+function gitRandomArray(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+var generatePassword = function() {
+  do {
+  var passwordLength = parseInt(window.prompt("How many characters do you want you passord to be between 8 and 128?"));
+  }while (passwordLength < 8 || passwordLength > 128);
+
+  var passwordCase = window.confirm("Do you want to have an upper case letter in your password?");
+
+  var passwordNumeric = window.confirm("Do you want to have numbers in your password?");
+
+  var passwordSpecialChar = window.confirm("Do you want to have any special charaters in your password?");
+
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  var numeric = [0,1,2,3,4,5,6,7,8,9];
+
+  var specialChar = ["!", "@", "#", "$", "%", "&", "*", "-", "+"];
+
+  var randomShit = [lowerCase, upperCase, numeric, specialChar];
+
+  for (var i = 0; i < passwordLength; i++){
+     console.log(randomShit[gitRandomArray(0,3)]);
+    
+    
+    
+  }
+
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
